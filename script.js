8f7bfbe8982fdbf1aae27e32a8eb49fbdf1ffaa4
charsides <charlotte.sides@gmail.com>
2020-03-15 20:13:34 +0000

    Name input updated

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 
 const button = document.getElementById("addButton");
-const ul = document.getElementsByTagName('ul')[0];
+const ul = document.getElementsByTagName("ul")[0];
 const newItem = document.getElementById("input");
+const username = document.getElementById("fname");
+const list = document.getElementsByClassName("container")[0];
+const submit = document.getElementById("submitName");
+const wrapper = document.getElementsByClassName("wrapper")[0];
+const body = document.getElementsByTagName("BODY")[0];
+const toggle = document.getElementsByClassName("switch")[0];
 
 // ask user for their name //
 
-function getUsername(){
-    var inputBox;
-    inputBox = prompt('Welcome, what\'s your name?');
-    if (inputBox === null) {
+function getUsername() {
+    if (username === null) {
+        console.log('no name');
         insertName("");
-    }else{
-        insertName(inputBox);
+    } else {
+        console.log(username.value)
+        insertName(username.value);
     }
+    setTimeout( function() {
+        wrapper.style.display = "none";
+        body.style.backgroundColor = "white";
+        list.style.display = "block";
+    }, 500);
 }
+
+submit.addEventListener('click', getUsername);
+username.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      submit.click();
+    }
+  });
 
 // insert user's name into heading //
 
@@ -24,9 +43,6 @@ function insertName (name) {
     } else {
         heading.innerHTML = name.charAt(0).toUpperCase() + name.slice(1) + "'s To-Do List";
     }
-}
-window.onload = function() {
-    getUsername();
 }
 
 // add remove button to each new item on the list //
@@ -63,14 +79,16 @@ function attachDownButtons(li) {
 function addItem () {
     let li = document.createElement("li");
     if (newItem.value != "") {
-        li.innerHTML = newItem.value;
+        li.innerHTML = newItem.value.charAt(0).toUpperCase() + newItem.value.slice(1)
         attachRemoveButtons(li);
         attachDownButtons(li);
         attachUpButtons(li);
         ul.appendChild(li);
         newItem.value = "";
+    } else {
+        alert('Oops! Please enter an item.');
     }
-};
+}
 
 button.addEventListener('click', addItem);
 
@@ -81,7 +99,7 @@ newItem.addEventListener("keyup", function(event) {
   }
 });
 
-// remove item from the list when remove button is clicked //
+// remove item from the list when remove button is clicked, move items up and down //
 
 ul.addEventListener('click',(event) => {
     if (event.target.id == 'removeButton') {
@@ -104,3 +122,5 @@ ul.addEventListener('click',(event) => {
         }
     }
 }); 
+
+
